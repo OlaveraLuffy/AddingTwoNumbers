7acf2095495f9ae9326cc1494c0a51a32fe13310
Ariel Arevalo <38834838+OlaveraLuffy@users.noreply.github.com>
2020-07-29 19:51:43 +0800

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 29 19:43:00 2020
+
+@author: Ariel Arevalo
+"""
+
+
+num1 = 2
+num2 = 5
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
